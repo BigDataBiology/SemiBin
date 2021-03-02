e1a8cace3766e56c799c9b375a10e61f7a483153
psj1997 <1176894699@qq.com>
2021-03-02 12:42:20 +0800

    Add Automatic test

---
+++ test/test_cov.py
@@ -0,0 +1,48 @@
+from s3n2bin.generate_coverage import calculate_coverage
+from os import path
+import pytest
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+
+def test_cov(tmpdir):
+    depth_file = path.join(tmpdir, 'depth.txt')
+
+    with open(depth_file, 'wt') as out_depth:
+        out_depth.write('k141_63080\t0\t6\t1\n')
+        out_depth.write('k141_63080\t6\t30\t2\n')
+        out_depth.write('k141_63080\t30\t138\t3\n')
+        out_depth.write('k141_63080\t138\t144\t2\n')
+        out_depth.write('k141_63080\t144\t174\t1\n')
+        out_depth.write('k141_63080\t174\t177\t2\n')
+        out_depth.write('k141_63080\t177\t257\t3\n')
+        out_depth.write('k141_63080\t257\t304\t2\n')
+        out_depth.write('k141_63080\t304\t315\t1\n')
+        out_depth.write('k141_0\t0\t45\t1\n')
+        out_depth.write('k141_0\t45\t88\t2\n')
+        out_depth.write('k141_0\t88\t138\t3\n')
+        out_depth.write('k141_0\t138\t159\t2\n')
+        out_depth.write('k141_0\t159\t183\t3\n')
+        out_depth.write('k141_0\t183\t225\t2\n')
+        out_depth.write('k141_0\t225\t227\t1\n')
+        out_depth.write('k141_0\t227\t297\t3\n')
+        out_depth.write('k141_0\t297\t338\t2\n')
+        out_depth.write('k141_0\t338\t365\t1\n')
+
+    cov, cov_split = calculate_coverage(
+        depth_file,
+        threshold=0,
+        is_combined=True,
+        contig_threshold=0)
+    assert_frame_equal(cov, pd.DataFrame([2.581818, 2.627907], index=[
+                       'k141_63080', 'k141_0'], columns=['cov']))
+    assert_frame_equal(cov_split, pd.DataFrame([2.609756, 2.554217, 2.682243, 2.574074], index=[
+                       'k141_63080_1', 'k141_63080_2', 'k141_0_1', 'k141_0_2'], columns=['cov']))
+
+    cov = calculate_coverage(
+        depth_file,
+        threshold=0,
+        is_combined=False,
+        contig_threshold=0)
+    assert_frame_equal(cov, pd.DataFrame([[2.581818, 0.606942], [
+                       2.627907, 0.252244]], index=['k141_63080', 'k141_0'], columns=['mean', 'var']))
---
+++ test/test_kmer.py
@@ -0,0 +1,45 @@
+from s3n2bin.generate_kmer import generate_kmer_features_from_fasta
+from os import path
+import pytest
+import pandas as pd
+from pandas.testing import assert_frame_equal
+import numpy as np
+
+
+def test_kmer(tmpdir):
+    fasta_file = path.join(tmpdir, 'contig.fna')
+
+    with open(fasta_file, 'wt') as out_fasta:
+        out_fasta.write(f'>example\nGCATGAGACAGTGTTTGCTGCAATACTCCCTGAGGCGCTTGCATATTATGAGAAGAATGCTCCCAAGGGAGAGTGCGTGATCGTTATAGAGGGAAAGAGCAGGCTTGAGATTCGGGAAGAAGAGAAAGCACAGTGGGAACAGCTTACCATTGAAGAACATATGGAACGCTATCTCTCCGGCGGTATGGAGAAAAAGGAAGCTATGAAGCGGGTGGCAAAGGATCGGGGCGTAAGTAAAAGAGATATTTATCAAGCGCTTCTTTAAACCTTCATATGAATGAAATAAAAACAGGTATCGGGTGTGTTC\n')
+
+    kmer = generate_kmer_features_from_fasta(
+        fasta_file, 0, kmer_len=1, split=False, threshold=0)
+    assert_frame_equal(kmer, pd.DataFrame(
+        np.array([0.547231, 0.452769]).reshape(1, 2), index=['example']))
+
+    kmer_split = generate_kmer_features_from_fasta(
+        fasta_file, 0, kmer_len=1, split=True, threshold=0)
+    assert_frame_equal(kmer_split, pd.DataFrame(
+        [[0.516340, 0.483660], [0.577922, 0.422078]], index=['example_1', 'example_2']))
+
+    kmer = generate_kmer_features_from_fasta(
+        fasta_file, 0, kmer_len=2, split=False, threshold=0)
+    assert_frame_equal(kmer,
+                       pd.DataFrame(np.array([0.169935,
+                                              0.078431,
+                                              0.147059,
+                                              0.07516340680934419,
+                                              0.058823542868123246,
+                                              0.117647,
+                                              0.143791,
+                                              0.104575,
+                                              0.06535948844461162,
+                                              0.03921570613865813]).reshape(1, 10),
+                                    index=['example']))
+
+    kmer_split = generate_kmer_features_from_fasta(
+        fasta_file, 0, kmer_len=2, split=True, threshold=0)
+    assert_frame_equal(kmer_split,
+                       pd.DataFrame([[0.131579, 0.06578949619112093, 0.177632, 0.07236843923128998, 0.03947372403044471, 0.151316, 0.157895, 0.098684, 0.07894738227145903, 0.02631583795010661],
+                                    [0.202614, 0.0915032735272722, 0.117647, 0.07843138664615251, 0.07843138664615251, 0.084967, 0.130719, 0.111111, 0.05228761288391314, 0.05228761288391314]],
+                                                index=['example_1', 'example_2']))
