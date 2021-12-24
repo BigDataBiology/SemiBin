7420385c2268c305d499537416a78e88c854ceb1
Luis Pedro Coelho <luis@luispedro.org>
2021-12-24 09:17:05 +0100

    TST Refactor tests
    Move integration tests to their own directory

---
+++ integration-tests/README.md
@@ -0,0 +1,4 @@
+#Integration tests
+
+These rely on `SemiBin` being available on the `$PATH`
+and run full pipelines.
---
+++ integration-tests/bin_command.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+
+# Test different input formats
+for ifile, odir in [
+        ('input.fasta', 'output_bin_fa'),
+        ('input.fasta.gz', 'output_bin_gz'),
+        ('input.fasta.bz2', 'output_bin_bz2'),
+        ('input.fasta.xz', 'output_bin_xz'),
+        ]:
+    subprocess.check_call(
+        ['SemiBin', 'bin',
+         '--data', 'test/bin_data/data.csv',
+         '--minfasta-kbs', '200',
+         '--recluster',
+         '--max-edges', '20',
+         '--max-node', '1',
+         '--model', 'test/bin_data/model.h5',
+         '-i', f'test/bin_data/{ifile}',
+         '-o', odir,
+         '-m', '2500',
+         '--ratio', '0.05',
+         '-p', '1'])
+    assert len(os.listdir(f'{odir}/output_bins')) > 0
+    assert len(os.listdir(f'{odir}/output_recluster_bins')) > 0
+
+# Different pretrained models
+for env,odir in [
+        ('human_gut', 'output_human_gut'),
+        ('dog_gut', 'output_dog_gut'),
+        ('ocean', 'output_ocean'),
+        ]:
+    subprocess.check_call(
+        ['SemiBin', 'bin',
+         '--data', 'test/bin_data/data.csv',
+         '--minfasta-kbs', '200',
+         '--recluster',
+         '--max-edges', '20',
+         '--max-node', '1',
+         '--environment', env,
+         '-i', 'test/bin_data/input.fasta.xz',
+         '-o', odir,
+         '-m', '2500',
+         '--ratio', '0.05',
+         '-p', '1'])
+    assert len(os.listdir(odir+'/output_bins')) > 0
+    assert len(os.listdir(odir+'/output_recluster_bins')) > 0
