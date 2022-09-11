ce07b3ab97ff2ef916472b67a06c4ad85b3547ba
Luis Pedro Coelho <luis@luispedro.org>
2022-09-11 19:15:00 +0200

    TST Refactor test
    Also, output into subdirectory

--- integration-tests/generate_data_coassembly_command.py
+++ integration-tests/generate_data_coassembly_command.py
@@ -1,41 +1,28 @@
 import os
 import pandas as pd
 import subprocess
+from glob import glob
+
+os.makedirs('test-outputs', exist_ok=True)
+bam_files = glob('test/coassembly_sample_data/input.sorted*.bam')
 
 
-### Input fa
-subprocess.check_call('SemiBin generate_sequence_features_single -i test/coassembly_sample_data/input.fasta -o output_coassembly_fa -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/coassembly_sample_data/input.sorted*.bam', shell=True)
+for ext in ['', '.gz', '.bz2', '.xz']:
+    odir = f"test-outputs/output_coassembly{'_'+ext.replace('.', '_') if ext else '_fa'}"
+    subprocess.check_call(
+        ['SemiBin', 'generate_sequence_features_single',
+         '-i', f'test/coassembly_sample_data/input.fasta{ext}',
+         '-o', odir,
+         '-m', '2500',
+         '--ratio', '0.05',
+         '--ml-threshold', '4000',
+         '-p', '1',
+         '-b'] + bam_files)
 
-data = pd.read_csv('output_coassembly_fa/data.csv', index_col=0)
-data_split = pd.read_csv('output_coassembly_fa/data_split.csv', index_col=0)
+    data = pd.read_csv(f'{odir}/data.csv', index_col=0)
+    data_split = pd.read_csv(f'{odir}/data_split.csv', index_col=0)
 
-assert data.shape == (40, 141)
-assert data_split.shape == (80, 141)
+    assert data.shape == (40, 141)
+    assert data_split.shape == (80, 141)
 
 
-### Input .gz
-subprocess.check_call('SemiBin generate_sequence_features_single -i test/coassembly_sample_data/input.fasta.gz -o output_coassembly_gz -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/coassembly_sample_data/input.sorted*.bam', shell=True)
-
-data = pd.read_csv('output_coassembly_gz/data.csv', index_col=0)
-data_split = pd.read_csv('output_coassembly_gz/data_split.csv', index_col=0)
-
-assert data.shape == (40, 141)
-assert data_split.shape == (80, 141)
-
-### Input .bz2
-subprocess.check_call('SemiBin generate_sequence_features_single -i test/coassembly_sample_data/input.fasta.bz2 -o output_coassembly_bz2 -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/coassembly_sample_data/input.sorted*.bam', shell=True)
-
-data = pd.read_csv('output_coassembly_bz2/data.csv', index_col=0)
-data_split = pd.read_csv('output_coassembly_bz2/data_split.csv', index_col=0)
-
-assert data.shape == (40, 141)
-assert data_split.shape == (80, 141)
-
-### Input .xz
-subprocess.check_call('SemiBin generate_sequence_features_single -i test/coassembly_sample_data/input.fasta.xz -o output_coassembly_xz -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/coassembly_sample_data/input.sorted*.bam', shell=True)
-
-data = pd.read_csv('output_coassembly_xz/data.csv', index_col=0)
-data_split = pd.read_csv('output_coassembly_xz/data_split.csv', index_col=0)
-
-assert data.shape == (40, 141)
-assert data_split.shape == (80, 141)
