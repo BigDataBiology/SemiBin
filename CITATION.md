c82551b33c9fb12855dee9edecdcd7bb9b85938a
Luis Pedro Coelho <luis@luispedro.org>
2022-12-10 16:04:20 +0800

    MIN More complete citation

--- CITATION.md
+++ CITATION.md
@@ -1,3 +1,6 @@
 If you use this software in a publication please cite:
 
->  Pan, S., Zhu, C., Zhao, XM. *et al.* A deep siamese neural network improves metagenome-assembled genomes in microbiome datasets across different environments. *Nat Commun* **13,** 2326 (2022). https://doi.org/10.1038/s41467-022-29843-y 
+>  Pan, S.; Zhu, C.; Zhao, XM.; Coelho, LP. A deep siamese neural network
+>  improves metagenome-assembled genomes in microbiome datasets across
+>  different environments. *Nat Commun* **13,** 2326 (2022).
+>  https://doi.org/10.1038/s41467-022-29843-y
--- SemiBin/main.py
+++ SemiBin/main.py
@@ -1367,7 +1367,7 @@ def main():
             from .utils import concatenate_fasta
             concatenate_fasta(args.contig_fasta, args.min_length, out, args.separator)
 
-        print('If you find SemiBin useful, please cite:\n Pan, S., Zhu, C., Zhao, XM. et al. A deep siamese neural network improves metagenome-assembled genomes in microbiome datasets across different environments. Nat Commun 13, 2326 (2022). https://doi.org/10.1038/s41467-022-29843-y.')
+        print('If you find SemiBin useful, please cite:\n Pan, S.; Zhu, C.; Zhao, XM.; Coelho, LP. A deep siamese neural network improves metagenome-assembled genomes in microbiome datasets across different environments. Nat Commun 13, 2326 (2022). https://doi.org/10.1038/s41467-022-29843-y.')
 
 if __name__ == '__main__':
     main()
