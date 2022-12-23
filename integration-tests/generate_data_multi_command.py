f6b3012dc08de01b2439520b89933f44062b4366
Luis Pedro Coelho <luis@luispedro.org>
2022-12-23 10:22:11 +0800

    TST Remove test outputs in top level directory
    Also, refactor tests and make them more exhaustive

--- integration-tests/generate_data_multi_command.py
+++ integration-tests/generate_data_multi_command.py
@@ -2,47 +2,47 @@ import subprocess
 import pandas as pd
 
 ### Input fa
-subprocess.check_call('SemiBin generate_sequence_features_multi -i test/multi_samples_data/input_multi.fasta -o output_multi_fa -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/multi_samples_data/input_multi_sorted*.bam -s :', shell=True)
+subprocess.check_call('SemiBin generate_sequence_features_multi -i test/multi_samples_data/input_multi.fasta -o test-outputs/output_multi_fa -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/multi_samples_data/input_multi_sorted*.bam -s :', shell=True)
 
 for i in range(10):
-    data = pd.read_csv('output_multi_fa/samples/S{}/data.csv'.format(i + 1),
+    data = pd.read_csv('test-outputs/output_multi_fa/samples/S{}/data.csv'.format(i + 1),
                        index_col=0)
     data_split = pd.read_csv(
-        'output_multi_fa/samples/S{}/data_split.csv'.format(i + 1), index_col=0)
+        'test-outputs/output_multi_fa/samples/S{}/data_split.csv'.format(i + 1), index_col=0)
     assert data.shape == (20, 146)
     assert data_split.shape == (40, 146)
 
 
 ### Input .gz
-subprocess.check_call('SemiBin generate_sequence_features_multi -i test/multi_samples_data/input_multi.fasta.gz -o output_multi_gz -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/multi_samples_data/input_multi_sorted*.bam -s :', shell=True)
+subprocess.check_call('SemiBin generate_sequence_features_multi -i test/multi_samples_data/input_multi.fasta.gz -o test-outputs/output_multi_gz -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/multi_samples_data/input_multi_sorted*.bam -s :', shell=True)
 
 for i in range(10):
-    data = pd.read_csv('output_multi_gz/samples/S{}/data.csv'.format(i + 1),
+    data = pd.read_csv('test-outputs/output_multi_gz/samples/S{}/data.csv'.format(i + 1),
                        index_col=0)
     data_split = pd.read_csv(
-        'output_multi_gz/samples/S{}/data_split.csv'.format(i + 1), index_col=0)
+        'test-outputs/output_multi_gz/samples/S{}/data_split.csv'.format(i + 1), index_col=0)
     assert data.shape == (20, 146)
     assert data_split.shape == (40, 146)
 
 ### Input .bz2
-subprocess.check_call('SemiBin generate_sequence_features_multi -i test/multi_samples_data/input_multi.fasta.bz2 -o output_multi_bz2 -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/multi_samples_data/input_multi_sorted*.bam -s :', shell=True)
+subprocess.check_call('SemiBin generate_sequence_features_multi -i test/multi_samples_data/input_multi.fasta.bz2 -o test-outputs/output_multi_bz2 -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/multi_samples_data/input_multi_sorted*.bam -s :', shell=True)
 
 for i in range(10):
-    data = pd.read_csv('output_multi_bz2/samples/S{}/data.csv'.format(i + 1),
+    data = pd.read_csv('test-outputs/output_multi_bz2/samples/S{}/data.csv'.format(i + 1),
                        index_col=0)
     data_split = pd.read_csv(
-        'output_multi_bz2/samples/S{}/data_split.csv'.format(i + 1), index_col=0)
+        'test-outputs/output_multi_bz2/samples/S{}/data_split.csv'.format(i + 1), index_col=0)
     assert data.shape == (20, 146)
     assert data_split.shape == (40, 146)
 
 ### Input .xz
-subprocess.check_call('SemiBin generate_sequence_features_multi -i test/multi_samples_data/input_multi.fasta.xz -o output_multi_xz -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/multi_samples_data/input_multi_sorted*.bam -s :', shell=True)
+subprocess.check_call('SemiBin generate_sequence_features_multi -i test/multi_samples_data/input_multi.fasta.xz -o test-outputs/output_multi_xz -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/multi_samples_data/input_multi_sorted*.bam -s :', shell=True)
 
 for i in range(10):
-    data = pd.read_csv('output_multi_xz/samples/S{}/data.csv'.format(i + 1),
+    data = pd.read_csv('test-outputs/output_multi_xz/samples/S{}/data.csv'.format(i + 1),
                        index_col=0)
     data_split = pd.read_csv(
-        'output_multi_xz/samples/S{}/data_split.csv'.format(i + 1), index_col=0)
+        'test-outputs/output_multi_xz/samples/S{}/data_split.csv'.format(i + 1), index_col=0)
     assert data.shape == (20, 146)
     assert data_split.shape == (40, 146)
 
--- integration-tests/generate_data_single_command.py
+++ integration-tests/generate_data_single_command.py
@@ -1,42 +1,34 @@
 import pandas as pd
 import subprocess
 
-### Input fa
-subprocess.check_call('SemiBin generate_sequence_features_single -i test/single_sample_data/input.fasta -o output_single_fa -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/single_sample_data/input.sorted.bam', shell=True)
+data0 = None
+data0_split = None
+for ifile in [
+        'input.fasta',
+        'input.fasta.gz',
+        'input.fasta.bz2',
+        'input.fasta.xz',
+        ]:
+    odir_suf = ifile.split('.')[1]
+    subprocess.check_call(
+        ['SemiBin', 'generate_sequence_features_single',
+         '-i', f'test/single_sample_data/{ifile}',
+         '-o', f'test-outputs/output_single_{odir_suf}',
+         '-m', '2500',
+         '--ratio', '0.05',
+         '--ml-threshold', '4000',
+         '-p', '1',
+         '-b', 'test/single_sample_data/input.sorted.bam'])
 
-data = pd.read_csv('output_single_fa/data.csv', index_col=0)
-data_split = pd.read_csv('output_single_fa/data_split.csv', index_col=0)
+    data = pd.read_csv(f'test-outputs/output_single_{odir_suf}/data.csv', index_col=0)
+    data_split = pd.read_csv(f'test-outputs/output_single_{odir_suf}/data_split.csv', index_col=0)
 
-assert data.shape == (40, 138)
-assert data_split.shape == (80, 136)
-
-
-### Input .gz
-subprocess.check_call('SemiBin generate_sequence_features_single -i test/single_sample_data/input.fasta.gz -o output_single_gz -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/single_sample_data/input.sorted.bam', shell=True)
-
-data = pd.read_csv('output_single_gz/data.csv', index_col=0)
-data_split = pd.read_csv('output_single_gz/data_split.csv', index_col=0)
-
-assert data.shape == (40, 138)
-assert data_split.shape == (80, 136)
-
-### Input .bz2
-subprocess.check_call('SemiBin generate_sequence_features_single -i test/single_sample_data/input.fasta.bz2 -o output_single_bz2 -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/single_sample_data/input.sorted.bam', shell=True)
-
-data = pd.read_csv('output_single_bz2/data.csv', index_col=0)
-data_split = pd.read_csv('output_single_bz2/data_split.csv', index_col=0)
-
-assert data.shape == (40, 138)
-assert data_split.shape == (80, 136)
-
-### Input .xz
-subprocess.check_call('SemiBin generate_sequence_features_single -i test/single_sample_data/input.fasta.xz -o output_single_xz -m 2500 --ratio 0.05 --ml-threshold 4000 -p 1 -b test/single_sample_data/input.sorted.bam', shell=True)
-
-data = pd.read_csv('output_single_xz/data.csv', index_col=0)
-data_split = pd.read_csv('output_single_xz/data_split.csv', index_col=0)
-
-assert data.shape == (40, 138)
-assert data_split.shape == (80, 136)
-
-
+    assert data.shape == (40, 138)
+    assert data_split.shape == (80, 136)
+    if data0 is not None:
+        assert (data == data0).all().all()
+        assert (data_split == data0_split).all().all()
+    else:
+        data0 = data
+        data0_split = data_split
 
