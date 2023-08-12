e6e89185db6a2550db134e81a6f1bc59cac0f015
Luis Pedro Coelho <luis@luispedro.org>
2023-08-12 12:01:07 +0100

    TST Add regression test for gh-137
    If the last batch is exactly of size 1, then SemiBin produces an error.
    
    /cc #137

--- test/test_train.py
+++ test/test_train.py
@@ -48,3 +48,54 @@ def test_train_self(tmpdir):
             )
 
     assert os.path.exists(f'{odir}/model.h5')
+
+
+# https://github.com/BigDataBiology/SemiBin/issues/137
+def test_regression_137_semi(tmpdir):
+    from SemiBin.semi_supervised_model import train
+    odir = f'{tmpdir}/output_train_semi'
+    os.makedirs(odir)
+    # 40 elements plus header: 41
+    assert len(open('test/train_data/data.csv', 'r').readlines()) == 41
+    model = train(odir,
+                       contig_fastas = ['test/train_data/input.fasta'],
+                       logger = logging,
+                       binned_lengths = [1000],
+                       datas=['test/train_data/data.csv'],
+                       data_splits=['test/train_data/data_split.csv'],
+                       cannot_links = ['test/train_data/cannot.txt'],
+                       is_combined=False,
+                       batchsize=39, # test/semi_data/data.csv has 40 elements so bug is triggered when batchsize is 39
+                       epoches=1,
+                       device='cpu',
+                       num_process=1,
+                       mode='single',
+                       )
+    assert os.path.exists(f'{odir}/model.h5')
+
+
+# https://github.com/BigDataBiology/SemiBin/issues/137
+def test_regression_137_self(tmpdir):
+    from SemiBin.self_supervised_model import train_self
+    odir = f'{tmpdir}/output_train_self'
+    os.makedirs(odir)
+    # 40 elements plus header: 41
+    assert len(open('test/train_data/data.csv', 'r').readlines()) == 41
+    # 80 elements plus header: 81
+    assert len(open('test/train_data/data_split.csv', 'r').readlines()) == 81
+
+    # Training adds len(<split>) * 1000//2 + 40 so that the total data is 40040
+    # To trigger the bug, batchsize is set to 40039
+    model = train_self(out = odir,
+                       logger = logging,
+                       datapaths=['test/train_data/data.csv'],
+                       data_splits=['test/train_data/data_split.csv'],
+                       is_combined=False,
+                       batchsize=40039,
+                       epoches=1,
+                       device='cpu',
+                       num_process=1,
+                       mode='single',
+                       )
+    assert os.path.exists(f'{odir}/model.h5')
+
