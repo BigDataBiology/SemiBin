23a246efddaa5370136b511723943ee7ddde8b3e
Luis Pedro Coelho <luis@luispedro.org>
2023-10-21 15:28:23 +1000

    MIN update description

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = open('README.md', encoding='utf-8').read()
 setup(name='SemiBin',
       version=__version__,
       python_requires='>=3.7',
-      description='Metagenomic binning with semi-supervised siamese neural network',
+      description='Metagenomic binning with siamese neural networks',
       long_description = long_description,
       long_description_content_type = 'text/markdown',
       classifiers=[
