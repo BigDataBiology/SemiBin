e05f3d518bfe37af1c35fe7888d7f8ba1198a405
Luis Pedro Coelho <luis@luispedro.org>
2023-11-03 14:44:41 +1000

    RFCT Avoid copies/temp when building train data

--- SemiBin/self_supervised_model.py
+++ SemiBin/self_supervised_model.py
@@ -56,7 +56,6 @@ def train_self(logger, out : str, datapaths, data_splits, is_combined=True,
                 logger.debug(f'Reading training data for index {data_index}...')
 
             data = pd.read_csv(datapath, index_col=0)
-            data.index = data.index.astype(str)
             data_split = pd.read_csv(data_split_path, index_col=0)
 
             if mode == 'several':
@@ -66,58 +65,47 @@ def train_self(logger, out : str, datapaths, data_splits, is_combined=True,
                     sys.exit(1)
 
             train_data = data.values
-            train_data_must_link = data_split.values
+            train_data_split = data_split.values
 
             if not is_combined:
-                train_data_input = train_data[:, 0:136]
-                train_data_split_input = train_data_must_link
+                train_data = train_data[:, :136]
             else:
-                train_data_input = train_data
-                train_data_split_input = train_data_must_link
-
-                if train_data_input.shape[1] - 136 > 20:
-                    train_data_kmer = train_data_input[:,0:136]
-                    train_data_depth = train_data_input[:, 136:len(data.values[0])]
+                if train_data.shape[1] - 136 > 20:
+                    train_data_kmer  = train_data[:, :136]
+                    train_data_depth = train_data[:, 136:]
                     train_data_depth = normalize(train_data_depth, axis=1, norm='l1')
-                    train_data_input = np.concatenate((train_data_kmer, train_data_depth), axis=1)
+                    train_data = np.concatenate((train_data_kmer, train_data_depth), axis=1)
 
-                    train_data_split_kmer = train_data_split_input[:,0:136]
-                    train_data_split_depth = train_data_split_input[:, 136:len(data.values[0])]
+                    train_data_split_kmer  = train_data_split[:, :136]
+                    train_data_split_depth = train_data_split[:, 136:]
                     train_data_split_depth = normalize(train_data_split_depth, axis=1, norm='l1')
-                    train_data_split_input = np.concatenate((train_data_split_kmer, train_data_split_depth), axis = 1)
+                    train_data_split = np.concatenate((train_data_split_kmer, train_data_split_depth), axis = 1)
 
+            data_length = len(train_data)
 
-            data_length = len(train_data_input)
-
-            n_samples = min(len(train_data_split_input) * 1000 // 2, 4000000)
-            indices1 = np.random.choice(data_length, size=n_samples)
+            # cannot link data is sampled randomly
+            n_cannot_link = min(len(train_data_split) * 1000 // 2, 4_000_000)
+            indices1 = np.random.choice(data_length, size=n_cannot_link)
             indices2 = indices1 + 1 + np.random.choice(data_length - 1,
-                                                       size=n_samples)
+                                                       size=n_cannot_link)
             indices2 %= data_length
-            train_labels = [0] * n_samples
 
-            train_input_1 = []
-            train_input_2 = []
-
-            # must link from breaking up
-
-            for index1, index2 in zip(indices1, indices2):
-                train_input_1.append(train_data_input[index1])
-                train_input_2.append(train_data_input[index2])
-
-            for i in range(0, len(train_data_must_link), 2):
-                train_input_1.append(train_data_split_input[i])
-                train_input_2.append(train_data_split_input[i + 1])
-                train_labels.append(1)
 
             if epoch == 0:
                 logger.debug(
-                    'Number of must-link pairs:{}'.format(train_labels.count(1)))
+                    f'Number of must-link pairs: {len(train_data_split)//2}')
                 logger.debug(
-                    'Number of cannot-link pairs:{}'.format(train_labels.count(0)))
+                    f'Number of cannot-link pairs: {n_cannot_link}')
 
-            dataset = feature_Dataset(train_input_1, train_input_2,
-                                      train_labels)
+            train_input_1 = np.concatenate(
+                                (train_data[indices1],
+                                train_data_split[::2]))
+            train_input_2 = np.concatenate(
+                                    (train_data[indices2],
+                                    train_data_split[1::2]))
+            train_labels = np.zeros(len(train_input_1), dtype=np.float32)
+            train_labels[len(indices1):] = 1
+            dataset = feature_Dataset(train_input_1, train_input_2, train_labels)
             train_loader = DataLoader(
                 dataset=dataset,
                 batch_size=batchsize,
@@ -127,11 +115,11 @@ def train_self(logger, out : str, datapaths, data_splits, is_combined=True,
 
             for train_input1, train_input2, train_label in train_loader:
                 model.train()
-                train_input1 = train_input1.to(device)
-                train_input2 = train_input2.to(device)
-                train_label = train_label.to(device)
+                train_input1 = train_input1.to(device=device, dtype=torch.float32)
+                train_input2 = train_input2.to(device=device, dtype=torch.float32)
+                train_label = train_label.to(device=device, dtype=torch.float32)
                 embedding1, embedding2 = model.forward(
-                    train_input1.float(), train_input2.float())
+                    train_input1, train_input2)
                 # decoder1, decoder2 = model.decoder(embedding1, embedding2)
                 optimizer.zero_grad()
                 supervised_loss = loss_function(embedding1.double(), embedding2.double(), train_label.double())
