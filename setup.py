083215da360046ffd165d804b473be91e2ebd98f
SebastianDall <semoda@bio.aau.dk>
2025-01-28 09:54:33 +0100

    bump pymethylation_utils

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(name='SemiBin',
           'scikit-learn',
           'requests',
           'numexpr',
-          'pymethylation_utils==0.5.1',
+          'pymethylation_utils',
           'polars==1.19.0'
       ],
       package_data={
