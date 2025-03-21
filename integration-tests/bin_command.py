886913b48814cc0cd426ee46ec20d22cee875bf5
Luis Pedro Coelho <luis@luispedro.org>
2025-03-21 18:46:12 +1000

    TST Better integration test for bin commands
    - More checking of properties
    - Do not call with different compressed formats (other tests should
      sufficiently cover that SemiBin is compression agnostic for its
      input). This speeds up the test by about a 3rd

--- integration-tests/bin_command.py
+++ integration-tests/bin_command.py
@@ -1,55 +1,67 @@
 import os
+from os import path
 import subprocess
 import pandas as pd
 import shutil
 from glob import glob
 from SemiBin.fasta import fasta_iter
 
-def test_overlap(contig, bin_dir, prefix):
-    contig_dict = {}
-    for h, seq in fasta_iter(contig):
-        contig_dict[h] = seq
 
-    contig_bin_name = []
-    contig_bin_dict = {}
-    bin_list = os.listdir(bin_dir)
-    bin_list = [temp for temp in bin_list if temp.split('.')[-1] == '{}'.format(prefix)]
+def check_bin_output(fafile, odir):
+    contigs_original = dict(fasta_iter(fafile))
+    bin_info = pd.read_table(f'{odir}/contig_bins.tsv')
+    contig2bin = bin_info.set_index('contig').to_dict()['bin']
+    if path.exists(f'{odir}/bins_info.tsv'):
+        tab = pd.read_table(f'{odir}/bins_info.tsv', index_col=0)
+    else:
+        tab = pd.read_table(f'{odir}/recluster_bins_info.tsv', index_col=0)
+    for f in tab.index:
+        assert 'output_bins' in f
+        assert os.path.exists(f)
 
-    for bin in bin_list:
-        for h, seq in fasta_iter(
-                '{0}/{1}'.format(bin_dir, bin)):
-            contig_bin_name.append(h)
-            contig_bin_dict[h] = seq
+    contigs_binned = []
+    sequences_binned = {}
+    n_bins = 0
+    for bf in os.listdir(f'{odir}/output_bins/'):
+        if not bf.endswith('.gz'):
+            continue
+        n_bins += 1
+        bin_id = None
+        for h, seq in fasta_iter(f'{odir}/output_bins/{bf}'):
+            if bin_id is None:
+                bin_id = contig2bin[h]
+            else:
+                assert bin_id == contig2bin[h]
+            contigs_binned.append(h)
+            sequences_binned[h] = seq
+    assert len(set(bin_info['bin']) - set([-1])) == n_bins
 
-    assert len(contig_bin_name) == len(set(contig_bin_name))
+    # Check that contigs are not repeated across bins (or within a bin)
+    assert len(contigs_binned) == len(set(contigs_binned))
 
-    for contig in contig_bin_dict:
-        assert contig_dict[contig] == contig_bin_dict[contig]
+    # Check that contigs do not change (and no new contigs are added)
+    for contig in sequences_binned:
+        assert contigs_original[contig] == sequences_binned[contig]
 
-# Test different input formats
-for ifile, odir in [
-        ('input.fasta', 'output_bin_fa'),
-        ('input.fasta.gz', 'output_bin_gz'),
-        ('input.fasta.bz2', 'output_bin_bz2'),
-        ('input.fasta.xz', 'output_bin_xz'),
-        ]:
-    odir = f'test-outputs/{odir}_no_recluster'
-    shutil.rmtree(odir, ignore_errors=True)
-    subprocess.check_call(
-        ['SemiBin2', 'bin_short',
-         '--data', 'test/bin_data/data.csv',
-         '--minfasta-kbs', '0',
-         '--max-edges', '20',
-         '--max-node', '1',
-         '--model', 'test/bin_data/model.pt',
-         '-i', f'test/bin_data/{ifile}',
-         '-o', odir,
-         '-m', '2500',
-         '--ratio', '0.05',
-         '-p', '1'])
 
-    assert len(os.listdir(f'{odir}/output_bins')) > 0
-    test_overlap(f'test/bin_data/{ifile}', f'{odir}/output_bins', "gz")
+ifile = 'input.fasta.xz'
+odir = 'test-outputs/output_bin_xz'
+shutil.rmtree(odir, ignore_errors=True)
+subprocess.check_call(
+    ['SemiBin2', 'bin_short',
+     '--data', 'test/bin_data/data.csv',
+     '--minfasta-kbs', '0',
+     '--max-edges', '20',
+     '--max-node', '1',
+     '--model', 'test/bin_data/model.pt',
+     '-i', f'test/bin_data/{ifile}',
+     '-o', odir,
+     '-m', '2500',
+     '--ratio', '0.05',])
+
+assert len(os.listdir(f'{odir}/output_bins')) > 0
+check_bin_output(f'test/bin_data/{ifile}', odir)
+
 
 odir = f'test-outputs/output_long'
 shutil.rmtree(odir, ignore_errors=True)
@@ -62,10 +74,10 @@ subprocess.check_call(
      '-o', odir,
      '-m', '2500',
      '--ratio', '0.05',
-     '--tag-output', 'TestTag',
-     '-p', '1'])
+     '--tag-output', 'TestTag'
+     ])
 assert len(glob(f'{odir}/output_bins/TestTag_*')) > 0
-test_overlap(f'test/bin_data/input.fasta', f'{odir}/output_bins', "gz")
+check_bin_output(f'test/bin_data/input.fasta', odir)
 
 
 # Different pretrained models
@@ -75,6 +87,7 @@ for env,odir in [
         ('ocean', 'output_ocean'),
         ]:
     odir = f'test-outputs/{odir}'
+    shutil.rmtree(odir, ignore_errors=True)
     subprocess.check_call(
         ['SemiBin2', 'bin_short',
          '--data', 'test/bin_data/data.csv',
@@ -82,15 +95,11 @@ for env,odir in [
          '--max-edges', '20',
          '--max-node', '1',
          '--environment', env,
-         '-i', 'test/bin_data/input.fasta.xz',
+         '-i', 'test/bin_data/input.fasta',
          '-o', odir,
          '-m', '2500',
-         '--ratio', '0.05',
-         '-p', '1'])
+         '--ratio', '0.05'])
     assert len(os.listdir(odir+'/output_bins')) > 0
-    tab = pd.read_table(f'{odir}/recluster_bins_info.tsv', index_col=0)
-    for f in tab.index:
-        assert 'output_bins' in f
-        assert os.path.exists(f)
+    check_bin_output('test/bin_data/input.fasta', odir)
 
 
